17bb60f9915c9b5f780dd20191dc22d541ca0782
Tanush <tanushbhattmains@gmail.com>
2026-06-24 22:09:42 +0530

    Commit P1.1 and P1.2

---
+++ src/Prac_1.1.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var a : Int
+    a = 22
+    var b : Float
+    b = 1.5F
+    var c : Char
+    c = 'T'
+    var d : String
+    d = "Tanush is the best!"
+    var e : Boolean
+    e = false
+    var f : Double
+    f = 125.9898
+    var g : Long
+    g = 123456789
+    var h : Short
+    h = -3
+    var i : Byte
+    i = 126
+    println("Integer value: " + a)
+    println("Float value: " + b)
+    println("Character value: " + c)
+    println("String value: " + d)
+    println("Boolean value: " + e)
+    println("Double value: " + f)
+    println("Long value: " + g)
+    println("Short value: " + h)
+    println("Byte value: " + i)
+
+//    println(a)
+//    println("value is: $e")
+//    val price = 19.99
+//    val item = "Book"
+//
+//    println("%s costs $%.2f".format(item, price))
+
+}
---
+++ src/Prac_1.2.kt
@@ -0,0 +1,20 @@
+fun main(){
+//    val intNum = 5
+//    val floatNum : Float = intNum.toFloat()
+//    println(intNum)
+//    println(floatNum)
+    val i  = 10000
+    val j : Double = i.toDouble()
+    val k : String = i.toString()
+    var a : String
+    a = "10"
+    val z = "11.12"
+    val b : Int = a.toInt()
+    val c : Double = z.toDouble()
+    println("Integer value: $i")
+    println("Double value(From Integer): $j")
+    println("String value: $a")
+    println("Integer value(From String): $b")
+    println("Double value(From String): $c")
+
+}
